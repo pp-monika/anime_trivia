6acb95be2bf61dfc006784b8f5434eed2bb2a367
Monika <97570594+pp-monika@users.noreply.github.com>
2022-10-13 21:27:09 -0700

    Update useQuestion.cpp
    - Add clarification
    - More Question's function use

--- useQuestion.cpp
+++ useQuestion.cpp
@@ -3,9 +3,15 @@
 
 int main(int argc, char *argv[]) {
 
+    std::cout << std::endl;
+
+    // Construct a question
     std::vector<std::string> t{"character", "FMA:B"};
     Question q(1, "Who is the protagonist of FMA:B?", "Edward Elric", t);
 
+
+    // Test getter functions
+    std::cout << "Test getter functions" << std::endl;
     std::cout << q.getId() << std::endl;
     std::cout << q.getQuestion() << std::endl;
     std::cout << q.getAnswer() << std::endl;
@@ -14,7 +20,42 @@ int main(int argc, char *argv[]) {
         std::cout << t1 << " ";
     }
     std::cout << std::endl;
+    std::cout << "------------------------------" << std::endl;
 
+
+    // Test setter functions
+    std::cout << "Test setter functions" << std::endl;
+    q.setId(2);
+    std::cout << q.getId() << std::endl;
+    q.setQuestion("What is Edward Elric's alchemist name?");
+    std::cout << q.getQuestion() << std::endl;
+    q.setAnswer("Fullmetal Alchemist");
+    std::cout << q.getAnswer() << std::endl;
+    std::cout << "------------------------------" << std::endl;
+
+
+    // Topics vector functions
+    std::cout << "Test topics vector functions" << std::endl;
+    q.addTopics("anime");
+    for (std::string t1 : q.getTopics()) {
+        std::cout << t1 << " ";
+    }
+    std::cout << std::endl;
+
+    q.removeTopics("FMA:B");
+    for (std::string t1 : q.getTopics()) {
+        std::cout << t1 << " ";
+    }
+    std::cout << std::endl;
+
+    q.clearTopics();
+    for (std::string t1 : q.getTopics()) {
+        std::cout << t1 << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "------------------------------" << std::endl;
+
+    std::cout << "Test print function" << std::endl;
     q.printQuestion();
 
 
